73ff185bde1eb00c7308f759a81b7c5cc187c8c9
Audun Rundberg <audun@rundberg.no>
2024-11-17 16:29:47 +0100

    build: no chunks

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,4 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes("highlight.js")) {
-            return "highlight";
-          }
-          if (id.includes("@reduxjs/toolkit") || id.includes("react-redux")) {
-            return "redux";
-          }
-          if (id.includes("react-hook-form")) {
-            return "react-hook-form";
-          }
-          return "vendors"; // Default chunk for other dependencies
-        },
-      },
-    },
-  },
 });
