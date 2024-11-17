f560813099f2fd6e59007610ac03d5c7ba7bcb1c
Audun Rundberg <audun@rundberg.no>
2024-11-17 16:29:46 +0100

    fix: ui changes

--- src/components/render-counter/counter.tsx
+++ src/components/render-counter/counter.tsx
@@ -5,5 +5,12 @@ export const Counter: React.FC = () => {
 
   count.current++;
 
-  return <p className="text-xl font-bold dark:text-gray-100">Render count: {count.current}</p>;
+  return (
+    <p className="text-xl dark:text-gray-100">
+      Render count:
+      <span className="inline-block w-8 h-8 text-center bg-purple-200 dark:bg-purple-600 dark:text-gray-100 rounded-full ml-3">
+        {count.current}
+      </span>
+    </p>
+  );
 };
