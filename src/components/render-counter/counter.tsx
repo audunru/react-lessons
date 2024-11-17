8951a96fc2ca88a5967535aaa8e1f02cbe72eedf
Audun Rundberg <audun@rundberg.no>
2024-11-17 16:29:47 +0100

    feat: counter color

--- src/components/render-counter/counter.tsx
+++ src/components/render-counter/counter.tsx
@@ -1,4 +1,25 @@
 import React, { useRef } from "react";
+import colors from "tailwindcss/colors";
+
+const hexToRgb = (hex: string) => {
+  const bigint = parseInt(hex.replace("#", ""), 16);
+  return {
+    r: (bigint >> 16) & 255,
+    g: (bigint >> 8) & 255,
+    b: bigint & 255,
+  };
+};
+
+const greenRgb = hexToRgb(colors.green[500]);
+const redRgb = hexToRgb(colors.red[500]);
+
+const getColor = (value: number) => {
+  const percentage = Math.min((value - 1) / 4, 1); // Transition between 1 and 5
+  const r = Math.round(greenRgb.r + (redRgb.r - greenRgb.r) * percentage).toString(10);
+  const g = Math.round(greenRgb.g + (redRgb.g - greenRgb.g) * percentage).toString(10);
+  const b = Math.round(greenRgb.b + (redRgb.b - greenRgb.b) * percentage).toString(10);
+  return `rgb(${r}, ${g}, ${b})`;
+};
 
 export const Counter: React.FC = () => {
   const count = useRef<number>(0);
@@ -8,7 +29,12 @@ export const Counter: React.FC = () => {
   return (
     <p className="text-xl dark:text-gray-100">
       Render count:
-      <span className="inline-block w-8 h-8 text-center bg-purple-200 dark:bg-purple-600 dark:text-gray-100 rounded-full ml-3">
+      <span
+        className="inline-block w-8 h-8 text-center text-gray-800 rounded-full ml-3"
+        style={{
+          backgroundColor: getColor(count.current),
+        }}
+      >
         {count.current}
       </span>
     </p>
