2fe125d69d4d2a5882dac4b533f47c080846f9d2
Audun Rundberg <audun@rundberg.no>
2024-11-17 16:29:46 +0100

    fix: padding

--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -3,7 +3,7 @@ import { Menu } from "../menu";
 
 export const Container: React.FC<PropsWithChildren> = (props) => {
   return (
-    <div className="container mx-auto px-4 mt-3">
+    <div className="container mx-auto px-4">
       <div className="flex flex-col lg:grid lg:grid-cols-[400px_auto] gap-y-6 lg:gap-y-0 lg:gap-x-20">
         <Menu />
         <div className="flex flex-col items-start gap-y-6">{props.children}</div>
