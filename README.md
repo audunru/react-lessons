49001ea3ea8b43dd1c4405b5c63ac44fe920a0cc
Audun Rundberg <audun@rundberg.no>
2024-11-17 16:29:47 +0100

    build: chunks

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 A collection of examples how common React patterns affect client side rendering.
 
 [View on GitHub Pages](https://audunru.github.io/react-lessons/)
+
+## Development
+
+Analyze bundle size:
+
+```bash
+ npx vite-bundle-visualizer -o stats.html --open
+```
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          redux: ["@reduxjs/toolkit", "react-redux"],
-          "react-hook-form": ["react-hook-form"],
-          highlight: ["highlight.js"],
+        manualChunks(id) {
+          if (id.includes("highlight.js")) {
+            return "highlight";
+          }
+          if (id.includes("@reduxjs/toolkit") || id.includes("react-redux")) {
+            return "redux";
+          }
+          if (id.includes("react-hook-form")) {
+            return "react-hook-form";
+          }
+          return "vendors"; // Default chunk for other dependencies
         },
       },
     },
